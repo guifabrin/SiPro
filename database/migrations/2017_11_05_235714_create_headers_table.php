f8d74eada5fccd76eb1986b084f7ea212755ebaf
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2017-11-06 11:10:41 -0200

    Alteração dos nomes dos arquivos de

---
+++ database/migrations/2017_11_05_235714_create_headers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Header extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('header', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('school');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('observation');
+            $table->boolean('teacher');
+            $table->boolean('date');
+            $table->double('value', 8, 2);
+            $table->text('imageb64');
+            $table->boolean('soft_delete');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('header');
+    }
+}
