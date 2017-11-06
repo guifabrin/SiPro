d9c6c9956406e674dca544583cb705683b4e35d2
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2017-11-06 10:48:43 -0200

    Alteração do modelo e do arquivo de  de alternativas.

--- app/Options.php
+++ app/Options.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Options extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'options';
+	
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'question_id', 'description', 'imageb64', 'correct','soft_delete'
+    ];
 }
