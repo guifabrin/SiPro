5c27b0f1ae4122602eeb99d6d555fd7e5ca7a270
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2017-11-06 14:27:23 -0200

    Adição do view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+    @if (Auth::check())
+        <div class="col-xs-12 col-sm-12 col-md-3 sidebar">
+            <div class="panel">
+                <div class="panel-heading">
+                    <h2>Menu Lateral</h2>
+                </div>
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
+                    @include('sidebar')
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 bodycontent">
+        @else
+        <div class="col-xs-12 col-md-12">
+        @endif
+            <div class="panel">
+                <div class="panel-heading">
+                    <div class="btnl">@yield('headerbtnl')</div>
+                    <h2>@yield('header')</h2>
+                    <div class="btnr">@yield('headerbtnr') </div>
+                </div>
+                <div class="panel-body">
+                @if (Auth::check())
+                    @if (!isset(Auth::user()->password) || Auth::user()->password=="")
+                        <div id="password_empty" class="alert alert-warning" role="alert"><p><b>Atenção:</b> como você registrou-se no sistema com uma conta do Facebook é necessário que você crie uma senha para o sistema.</p><center><a class="btn btn-sm btn-warning" href="{{ url('/home/user/password') }}" title="Criar senha">Criar senha</a></center></div>
                     @endif
-
-                    You are logged in!
+                @endif
+                    @if (session('message')!==null && session('message')!="")
+                        <div class="alert alert-{{session('status')}}" role="alert">{{session('message')}}</div>
+                        <?php
+                            \Session::forget('message');
+                        ?>
+                    @elseif (isset($message) && $message!="")
+                        <div class="alert alert-{{session('status')}}" role="alert">{{session('message')}}</div>
+                    @endif
+                    @yield('body')
                 </div>
             </div>
         </div>
