67d6efb1ece93b8c05a47cd6b74bfc85eb00137f
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2017-11-08 15:10:41 -0200

    Ajustado visualização e botão de voltar do menu do usuário.

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -3,17 +3,21 @@
 @section('my_account_active') active @endsection
 
 @section('headerbtnl')
-<a class="btn btn-sm btn-primary" href="{{ URL::previous() }}">
-<i class="fa fa-btn fa-arrow-circle-left"></i> Voltar
-</a> 
+	<a class="btn btn-sm btn-primary" href="{{ url('/') }}">
+		<i class="fa fa-btn fa-arrow-circle-left"></i> Voltar
+	</a>
 @endsection
 
 @section('header')
-Minha Conta
+	Minha Conta
 @endsection
 
 @section('body')
-<ul class="list-group">
-    <li class="list-group-item"><a href="{{ url('/user/password') }}"><i class="fa fa-key fa-btn"></i> Alterar senha de acesso</a></li>
-</ul>
+	<ul class="list-group">
+	    <li class="list-group-item">
+	    	<a href="{{ url('/user/password') }}">
+	    		<i class="fa fa-key fa-btn"></i> Alterar senha de acesso
+	    	</a>
+		</li>
+	</ul>
 @endsection
