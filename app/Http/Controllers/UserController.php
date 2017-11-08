6f29b877ca126b1d140380ff1abebaf9591286e5
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2017-11-08 15:08:39 -0200

    Adicionado comentários no UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller {
 
+	/**
+	 * Variáveis de caminho dos arquivos Blade
+	 */
 	private $userViewBlade = "user.view";
 	private $passwordFormBlade = "user.password.form";
 
+	/**
+	 * Array de mensagens de retorno das ações executadas no UserController
+	 */
 	private $messages = [
 		'ok' => [
 			'status' => 'success',
@@ -29,7 +35,7 @@ class UserController extends Controller {
 	];
 
 	/**
-	 * Create a new controller instance.
+	 * Construtor
 	 *
 	 * @return void
 	 */
@@ -37,14 +43,29 @@ class UserController extends Controller {
 		$this->middleware('auth');
 	}
 
+	/**
+	 * Função de retorno da página de configuração do usuário
+	 *
+	 * @return view
+	 */
 	public function read() {
 		return view($this->userViewBlade);
 	}
 
+	/**
+	 * Função de retorno da página de alteração de senha do usuário
+	 *
+	 * @return view
+	 */
 	public function passwordForm() {
 		return view($this->passwordFormBlade);
 	}
 
+	/**
+	 * Função de alteração de senha do usuário
+	 *
+	 * @return view
+	 */
 	public function updatePassword(Request $request) {
 		$args = ['id' => \Auth::user()->id];
 		$userQuery = User::where($args);
@@ -54,14 +75,19 @@ class UserController extends Controller {
 
 		$password = $input['password'];
 		$newPassword = $input['new-password'];
-		if (isset($input['old-password'])) {
+
+		$oldPasswordSetted = isset($input['old-password']);
+
+		//Caso o usuário tenha registrado com o Facebook não existirá uma senha antiga.
+		if ($oldPasswordSetted) {
 			$oldPassword = $input['old-password'];
 		}
 
 		if ($password == $newPassword) {
-			if ($user->password != "" && isset($oldPassword) && !\Hash::check($oldPassword, $user->password)) {
+			if ($user->password != "" && $oldPasswordSetted && !\Hash::check($oldPassword, $user->password)) {
 				return view($this->passwordFormBlade)->with('message', $this->messages['diff_old']);
 			}
+			//Caso a senha do usuário exista e a senha antiga esteja setada e corresponda a existente.
 			$bcryptPassword = bcrypt($password);
 			if ($userQuery->update(['password' => $bcryptPassword])) {
 				\Auth::user()->password = $bcryptPassword;
@@ -70,6 +96,7 @@ class UserController extends Controller {
 				return view($this->passwordFormBlade)->with('message', $this->messages['error']);
 			}
 		} else {
+			//Caso as novas senhas não correspondam.
 			return view($this->passwordFormBlade)->with('message', $this->messages['diff_new']);
 		}
 	}
