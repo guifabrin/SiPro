f162f0a5f9ff8882f5db33f4691112cafcad4e74
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-03-14 10:54:58 -0300

    Refactor Question Store and Account Service

---
+++ app/ProviderUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Laravel\Socialite\Contracts\User as SocialiteUser;
+
+class ProviderUser extends SocialiteUser
+{
+    public function toUserCreationArray(){
+        return [
+            "email" => $this->getEmail(),
+            "name" => $this->getName(),
+            "avatar" => $this->avatar,
+        ];
+    }
+}
