c17399b777bb096de431adadb593502fa17909ef
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-06-28 11:46:28 +0000

    Update framework laravel

--- app/Http/Middleware/CheckWordpress.php
+++ app/Http/Middleware/CheckWordpress.php
@@ -7,20 +7,67 @@ use App\User;
 use Closure;
 
 class CheckWordpress {
-    private function userFindOrCreate($id, $email) {
-        $user = User::where('wp_user_id', $id)->first();
-        if ($user) return $user;
-        $user = User::where('email', $email)->first();
-        if ($user) {
-            $user->update(['wp_user_id' => $id]);
-            return $user;
-        }
+
+    private function getUserWithWpUserId($wpUserId)
+    {
+        return User::where('wp_user_id', $wpUserId)->first();
+    }
+
+    private function getUserWithEmail($email)
+    {
+        return User::where('email', $email)->first();
+    }
+
+    private function getNewUser($wpUserId, $email)
+    {
         return User::create([
-            'email'=> $email,
-            'name'=> $email,
-            'wp_user_id' => $id
+            'name' => $email,
+            'email' => $email,
+            'wp_user_id' => $wpUserId
         ]);
     }
+
+    private function getUser($wpUserId, $email)
+    {
+        if ($user = $this->getUserWithWpUserId($wpUserId))
+            return $user;
+        if ($user = $this->getUserWithEmail($email)) {
+            $user->update(['wp_user_id' => $wpUserId]);
+            return $user;
+        }
+        return $this->getNewUser($wpUserId, $email);
+    }
+
+    private function loadDotEnv()
+    {
+        $dotenv = \Dotenv\Dotenv::create(__DIR__ . "/../../../");
+        $dotenv->load();
+    }
+
+    private function getWpLoadFile()
+    {
+        return env('WORDPRESS_PATH') . 'wp-load.php';
+    }
+
+    private function validWordpressIntegration()
+    {
+        return (env('WORDPRESS') && file_exists($this->getWpLoadFile()));
+    }
+
+    private function initWordpressIntegration()
+    {
+        if (!defined('WP_USE_THEMES')) define('WP_USE_THEMES', false);
+        include $this->getWpLoadFile();
+    }
+
+    private function loginWpUser($wpUserObject)
+    {
+        $user = $this->getUser($wpUserObject->ID, $wpUserObject->user_email);
+        $guest = Auth::guest();
+        $diffUser = !$guest && Auth::user()->id != $user->id;
+        if ($guest || $diffUser) Auth::login($user);
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -28,33 +75,16 @@ class CheckWordpress {
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next) {
-        $dotenv = \Dotenv\Dotenv::create(__DIR__ . "/../../../");
-        $dotenv->load();
-        $wpLoadFile = env('WORDPRESS_PATH') . 'wp-load.php';
-        if (env('WORDPRESS') && file_exists($wpLoadFile)) {
-            if (!defined('WP_USE_THEMES'))
-                define('WP_USE_THEMES', false);
-            include $wpLoadFile;
-            if (!is_user_logged_in())
-                return redirect(env('WORDPRESS_LOGIN_URL') . "?redirect_to=" . $request->url());
-            $user = wp_get_current_user();
-            $laravelUser = User::where('wp_user_id', $user->ID)->first();
-            if (Auth::check()) {
-                if (Auth::user()->id != $laravelUser->id) {
-                    Auth::logout();
-                    return redirect(env('WORDPRESS_LOGIN_URL') . "?redirect_to=" . $request->url());
-                }
-            } else {
-                if (!$laravelUser)
-                    $laravelUser = User::create([
-                        'email' => $user->data->user_email,
-                        'name' => $user->data->user_email,
-                        'wp_user_id' => $user->ID
-                    ]);
-                Auth::login($laravelUser);
-            }
-        }
+    public function handle($request, Closure $next)
+    {
+        $this->loadDotEnv();
+        if (!$this->validWordpressIntegration())
+            return $next($request);
+        $this->initWordpressIntegration();
+        if (!is_user_logged_in())
+            return redirect(env('WORDPRESS_LOGIN_URL'));
+        $user = wp_get_current_user();
+        $this->loginWpUser($user);
         return $next($request);
     }
 }
